a5f51015c83bddc5469ecba9f7c5741f304980de
Dilshan97 <dilshan70@outlook.com>
2018-12-26 15:01:11 +0530

    update background video

--- js/init.js
+++ js/init.js
@@ -74,7 +74,7 @@ $(function () {
 	};
 
 	// year/month/day
-	$('#countdown').countdown('2019/01/01', function (event) {
+	$('#countdown').countdown('2019/01/20', function (event) {
 		$(this).html(event.strftime(
 			'<div class="countdown-section"><b>%w</b> <span>' + description.weeks + '</span> </div>' +
 			'<div class="countdown-section"><b>%d</b> <span>' + description.days + '</span> </div>' +
